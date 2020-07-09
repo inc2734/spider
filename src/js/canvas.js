c8890b8e91914caf5a0864a5e134dac297c6fcda
Takashi Kitajima <inc@2inc.org>
2020-07-09 12:03:31 +0900

    Update updateCurrent()

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,3 +1,5 @@
+import addCustomEvent from '@inc2734/add-custom-event';
+
 export const Canvas = (canvas, args) => {
   canvas.scrollLeft = 0;
   canvas.setAttribute('data-current', 0);
@@ -18,40 +20,52 @@ export const Canvas = (canvas, args) => {
   };
 
   const scrollLock = () => {
-    const ajust = () => {
-      const canvasX   = canvas.getBoundingClientRect().left;
-      const itemXList = [];
-      [].slice.call(args.slides).forEach(
-        (item, index) => {
-          const itemX = item.getBoundingClientRect().left;
-          itemXList[ index ] = Math.abs(itemX - canvasX);
-        }
-      );
-
-      if (-1 !== itemXList.indexOf(0)) {
-        return;
+    const canvasX   = canvas.getBoundingClientRect().left;
+    const itemXList = [];
+    [].slice.call(args.slides).forEach(
+      (item, index) => {
+        const itemX = item.getBoundingClientRect().left;
+        itemXList[ index ] = Math.abs(itemX - canvasX);
       }
+    );
 
-      const min  = Math.min(...itemXList);
-      const near = itemXList.indexOf(min);
-      const nearItem  = args.slides[ near ];
-      const nearItemX = nearItem.getBoundingClientRect().left;
-      if (nearItem) {
-        canvas.scrollLeft += (nearItemX - canvasX);
-      }
-      canvasScrollTimerId = undefined;
-    };
-
-    if (canvasScrollTimerId) {
-      clearTimeout(canvasScrollTimerId);
+    if (-1 !== itemXList.indexOf(0)) {
+      return;
     }
 
-    canvasScrollTimerId = setTimeout(ajust, 100);
+    const min  = Math.min(...itemXList);
+    const near = itemXList.indexOf(min);
+    const nearItem  = args.slides[ near ];
+    const nearItemX = nearItem.getBoundingClientRect().left;
+    if (nearItem) {
+      canvas.scrollLeft += (nearItemX - canvasX);
+    }
   };
 
-  const handleScroll = () => {
-    updateCurrent();
-    scrollLock();
-  };
-  canvas.addEventListener('scroll', handleScroll, false);
+  let test = 0;
+  canvas.addEventListener(
+    'scroll',
+    () => {
+      clearTimeout(canvasScrollTimerId);
+
+      canvasScrollTimerId = setTimeout(
+        () => {
+          test ++;
+          console.log(test);
+          addCustomEvent(canvas, 'scrollEnd');
+        },
+        100
+      );
+    },
+    false
+  );
+
+  canvas.addEventListener(
+    'scrollEnd',
+    () => {
+      updateCurrent();
+      scrollLock();
+    },
+    false
+  );
 };
